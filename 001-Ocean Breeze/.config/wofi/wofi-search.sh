ee7a8297952134821abb70d92792ef79af4a847c
David Thievon <pdkfan@gmail.com>
2024-07-22 23:09:47 +0200

    feat(wofi): Improved wofi-search
    wofi-search.sh now checks if the tld is valid before assuming it is a
    url

--- 001-Ocean Breeze/.config/wofi/wofi-search.sh
+++ 001-Ocean Breeze/.config/wofi/wofi-search.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+TLD_PATH="${0%/*}/tlds.txt"
+
 term=$(wofi --show dmenu --height 50)
 
-if [[ "$term" =~ \.+ ]]; then
-	floorp --new-tab "$term"
-else
-	floorp --new-tab https://search.brave.com/search?q="$term"
+if [[ "$term" == *"."* ]]; then
+    tld="${term##*.}"
+    tld="${tld%%/*}"
+
+    echo "$tld"
+
+    if [[ -n "$(cat "$TLD_PATH" | grep -i -w $tld)" ]]; then
+        floorp --new-tab "$term"
+        exit
+    fi
 fi
+
+floorp --new-tab https://search.brave.com/search?q="$term"
